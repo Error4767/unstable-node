a66f1d6a4c0eae4e3c8092d8a79a27e1d2e5d60e
Error4767 <2303184060@qq.com>
2020-05-15 09:42:00 +0800

    add esm serverCanSetDirectory.js

---
+++ server/ESModule/serverCanSetDirectory.js
@@ -0,0 +1,71 @@
+import http from 'http';
+import fs from 'fs';
+//配置
+let config = {
+    basePath: './',//默认路径
+    mainHtmlFile: 'index.js'
+}
+let {
+    basePath = '',
+    mainHtmlFile = 'index.html'
+} = config;
+function getClientIp(req) {
+    return req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+};
+let contentTypes = {
+ "css": "text/css",
+ "gif": "image/gif",
+ "html": "text/html",
+ "ico": "image/x-icon",
+ "jpeg": "image/jpeg",
+ "jpg": "image/jpeg",
+ "js": "text/javascript",
+ "json": "application/json",
+ "pdf": "application/pdf",
+ "png": "image/png",
+ "svg": "image/svg+xml",
+ "swf": "application/x-shockwave-flash",
+ "tiff": "image/tiff",
+ "txt": "text/plain",
+ "wav": "audio/x-wav",
+ "wma": "audio/x-ms-wma",
+ "wmv": "video/x-ms-wmv",
+ "xml": "text/xml"
+}
+const server = http.createServer((request,response)=>{
+    let ip = getClientIp(request).substr(7);
+    let url = request.url;
+    // response.writeHead(200,{'Content-Type':'text/html'});
+    if(url=='/') {
+        response.writeHead(200,{'Content-Type':'text/html'});
+         let ns = fs.readFile(basePath + mainHtmlFile,(err, data)=>{
+             if (err) {
+                 //console.error(err);
+                 console.log(`访问请求 从${ip}发起 状态->失败 ${err}`);
+                 return;
+             }
+             console.log(`访问请求 从${ip}发起 状态->成功`);
+             response.end(data);
+         });
+    }else if(url != '/') {
+        //let surl = '.'+url;
+        let surl = url;
+        let type = surl.substr(surl.lastIndexOf(".")+1,surl.length);
+        type = contentTypes[type] ? contentTypes[type] : 'text/' + type;
+        response.writeHead(200,{'Content-type': type});
+        let ns = fs.readFile(basePath + surl,(err, data)=>{
+            if (err) {
+                //console.error(err);
+                console.log(`文件请求 ${url} 从${ip}发起 状态->失败 ${err}`);
+                return;
+            }
+            console.log(`文件请求 ${url} 从${ip}发起 状态->成功`);
+            response.end(data);
+        });
+    }
+
+});
+server.listen(8000);
