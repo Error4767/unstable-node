3c0c35abacf72b5a19efba98331740fa231d6041
Error4767 <2303184060@qq.com>
2020-06-21 09:56:50 +0800

    change to a package

---
+++ entry.js
@@ -0,0 +1,11 @@
+import PA from './src/module/PA.js';
+import pathPlus from './src/module/pathPlus.js';
+import readDirectoryTree from './src/module/readDirectoryTree.js';
+import fsPlus from './src/module/fsPlus.js';
+
+export {
+  PA,
+  pathPlus,
+  readDirectoryTree,
+  fsPlus
+}
