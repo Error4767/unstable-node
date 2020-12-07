0114e21413192de430f9f826de759ebceb10f0fd
Error4767 <2303184060@qq.com>
2020-12-07 14:39:17 +0800

    readDirectoryTree add directory attr (stat: { fileNumber, folderNumber, size })

--- src/module/readDirectoryTree.js
+++ src/module/readDirectoryTree.js
@@ -1,27 +1,45 @@
 import fs from 'fs';
 import pathPlus from './pathPlus.js';
 export default function readDirectoryTree(directory) {
-  return fs.promises.stat(directory).then(stat=> {
+  return fs.promises.stat(directory).then(stat => {
     if (stat.isDirectory()) {
-      return fs.promises.readdir(directory).then(files=> {
-        return Promise.allSettled(files.map(v=>readDirectoryTree(pathPlus.joinRelativePath(directory, v))));
-      }).then(r=> {
-        const dirInfo = r.map(v=> {
-          if(v.status === 'fulfilled') {
+      return fs.promises.readdir(directory).then(files => {
+        return Promise.allSettled(files.map(v => readDirectoryTree(pathPlus.joinRelativePath(directory, v))));
+      }).then(r => {
+        const dirInfo = r.map(v => {
+          if (v.status === 'fulfilled') {
             return v.value;
-          }else if(v.status === 'rejected'){
+          } else if (v.status === 'rejected') {
             return {
               type: 'error',
               error: v.reason
             };
           }
         });
+        let fileNumber = 0;
+        let folderNumber = 0;
+        let size = 0;
+        dirInfo.forEach((node) => {
+          size += node.stat.size;
+          if (node.type === 'file') {
+            fileNumber += 1;
+          } else if (node.type === 'directory') {
+            fileNumber += node.stat.fileNumber || 0;
+            // 加1是包括了此文件夹本身
+            folderNumber += (node.stat.folderNumber || 0) + 1;
+          }
+        }, 0);
         return {
           type: 'directory',
           directory,
-          children: dirInfo
+          children: dirInfo,
+          stat: {
+            fileNumber,
+            folderNumber,
+            size
+          }
         };
-      })
+      });
     } else {
       return {
         stat,
